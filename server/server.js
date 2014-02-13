38c7fb4d463e2ae8e8634c8211ad3e45be9e11d9
Kevin Fang <kfang@kfang.com>
2014-02-13 14:44:25 -0500

    Refactored client code into client folder

--- server/server.js
+++ server/server.js
@@ -8,3 +8,15 @@ Meteor.methods({
     Rooms.update(roomId, {$set: { videoTime: time }});
   }
 })
+
+// Function to auto-create a room on start up
+// Temporary hack until we support multiple rooms
+var initRoom = function () {
+  if (Rooms.find().count() == 0) {
+    Rooms.insert(new Room());
+  }
+}
+
+Meteor.startup(function () {
+  initRoom();
+});
